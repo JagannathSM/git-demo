4f06b715fd608474bb0ee0f59dfb1bd018e9ed10
JagannathSM <jagan7887@gmail.com>
2024-06-08 14:39:06 +0530

    updated

--- MODULE-2/task-2 shopping cart.js/src/App.jsx
+++ MODULE-2/task-2 shopping cart.js/src/App.jsx
@@ -1,15 +1,73 @@
-import { useState } from 'react'
 import './App.css'
-import { Routes, Route, Link} from "react-router-dom"
-import Cards from './Cards'
-import Navbar from './Navbar'
+import Footer from './Footer'
+import Header from './Header'
+
+export const data_list = [
+  {
+    sale:"",
+    name: "Fancy Product",
+    star:"",
+    price1 : "$40.00 - $80.00",
+    price2 : ""
+  },
+  {
+    sale:"sale",
+    name: "Special Item",
+    star:"star",
+    price1 : "$20.00",
+    price2 : "$18.00"
+  },
+  {
+    sale:"sale",
+    name: "Sale Item",
+    star:"",
+    price1 : "$50.00",
+    price2 : "$25.00"
+  },
+  {
+    sale:"",
+    name: "Popular Item",
+    star:"star",
+    price1 : "$40.00",
+    price2 : ""
+  },
+  {
+    sale:"sale",
+    name: "Sale Item",
+    star:"",
+    price1 : "$50.00",
+    price2 : "$25.00"
+  },
+  {
+    sale:"",
+    name: "Fancy Product",
+    star:"",
+    price1 : "$120.00 - $280.00",
+    price2 : ""
+  },
+  {
+    sale:"",
+    name: "Popular Item",
+    star:"star",
+    price1 : "$40.00",
+    price2 : ""
+  },
+  {
+    sale:"sale",
+    name: "Special Item",
+    star:"star",
+    price1 : "$20.00",
+    price2 : "$18.00"
+  }
+]
+
 
 function App() {
 
   return (
-    <div>
-      {/* <Cards/> */}
-      <Navbar/>
+    <div className='All-Container'>
+      <Header/>
+      <Footer/>
     </div>
   )
 }
