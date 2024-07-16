67db72a5ded61861ddad1a202e4fc1e85730c0ea
JagannathSM <jagan7887@gmail.com>
2024-07-16 19:58:16 +0530

    16-07-2024

--- Modulr-3_NodeJS/Node_Day_3_and_4/server.js
+++ Modulr-3_NodeJS/Node_Day_3_and_4/server.js
@@ -2,22 +2,30 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/index')
+const dotenv = require('dotenv')
+
+//Load Environment Variable from .env file
+//user_env = process.env.NODE_ENV
+//dotenv.config({path:`${user_env}`})
+dotenv.config();
 
 const app = express()
 app.use(bodyParser.json())
 
 //MongoDB Connectivity  mongodb+srv://guvi:guvi@guvi.mxnupan.mongodb.net/guvi // mongodb://localhost:27017/guvi
-mongoose.connect('mongodb+srv://guvi:guvi@guvi.mxnupan.mongodb.net/guvi')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB Connected Successfully")
 }).catch((error)=>{
     console.log('MongoDB Connection Failed')
 })
+// console.log(`MONGO_URI - ${process.env.MONGO_URI}`)
+
 
 //USE ROUTES
 app.use('/API', routes)
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`Server Running on Port - ${PORT}`)
 })
