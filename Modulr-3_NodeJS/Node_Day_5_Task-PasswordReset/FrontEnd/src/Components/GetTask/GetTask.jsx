cee8c44963a3db738d574309ae872b7f5358fbf1
JagannathSM <jagan7887@gmail.com>
2024-07-29 19:46:51 +0530

    29-07-2024

---
+++ Modulr-3_NodeJS/Node_Day_5_Task-PasswordReset/FrontEnd/src/Components/GetTask/GetTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GetTask() {
+  return (
+    <div>
+      GetTask
+    </div>
+  )
+}
+
+export default GetTask
---
+++ Modulr-3_NodeJS/Node_Day_5_Task-PasswordReset/FrontEnd/src/Components/NewPassword/NewPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewPassword() {
+  return (
+    <div>
+      NewPassword
+    </div>
+  )
+}
+
+export default NewPassword
